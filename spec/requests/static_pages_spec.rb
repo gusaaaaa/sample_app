32c13fb12f7069f334515fff8450c35de62dc29a
gusaaaaa <gusa@neo.com>
2012-12-20 16:02:55 -0200

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,6 +15,49 @@ describe "Static pages" do
 
 		it_should_behave_like "all static pages"
 		it { should_not have_selector 'title', text: '| Home' }
+
+		it "should have the right links on the layout" do
+			click_link "About"
+			page.should have_selector 'title', text: full_title('About Us')
+			click_link "Help"
+			page.should have_selector 'title', text: full_title('Help')
+			click_link "Contact"
+			page.should have_selector 'title', text: full_title('Contact')
+			click_link "Home"
+			click_link "Sign up now!"
+			page.should have_selector 'title', text: full_title('Sign up')
+			click_link "sample app"
+			page.should have_selector 'title', text: full_title('')
+		end
+
+		describe "for signed-in users" do
+			let(:user) { FactoryGirl.create(:user) }
+			before do
+				FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+				FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+				sign_in user
+				visit root_path
+			end
+			
+			it "should render the user's feed" do
+				user.feed.each do |item|
+					page.should have_selector("li##{item.id}", text: item.content)
+				end
+			end
+
+			describe "follower/following counts" do
+				let(:other_user) { FactoryGirl.create(:user) }
+				before do
+					other_user.follow!(user)
+					visit root_path
+				end
+
+				it { should have_link("0 following", href: following_user_path(user)) }
+				it { should have_link("1 followers", href: followers_user_path(user))}
+			end
+
+		end
+
   end
 
 	describe "Help page" do
@@ -36,37 +79,6 @@ describe "Static pages" do
 		let(:heading) { 'Contact' }
 		let(:page_title) { 'Contact' }
 		it_should_behave_like "all static pages"
-	end
-
-	it "should have the right links on the layout" do
-		visit root_path
-		click_link "About"
-		page.should have_selector 'title', text: full_title('About Us')
-		click_link "Help"
-		page.should have_selector 'title', text: full_title('Help')
-		click_link "Contact"
-		page.should have_selector 'title', text: full_title('Contact')
-		click_link "Home"
-		click_link "Sign up now!"
-		page.should have_selector 'title', text: full_title('Sign up')
-		click_link "sample app"
-		page.should have_selector 'title', text: full_title('')
-	end
-
-	describe "for signed-in users" do
-		let(:user) { FactoryGirl.create(:user) }
-		before do
-			FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-			FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-			sign_in user
-			visit root_path
-		end
-		
-		it "should render the user's feed" do
-			user.feed.each do |item|
-				page.should have_selector("li##{item.id}", text: item.content)
-			end
-		end
 	end
 
 end
